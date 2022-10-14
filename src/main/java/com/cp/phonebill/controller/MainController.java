ccd4cfe636f006966734bb4c28df25f228cd54fa
mayur238 <mayur.p@cloudpointamerica.com>
2022-10-14 14:37:44 +0530

    added controller package

---
+++ src/main/java/com/cp/phonebill/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.cp.phonebill.controller;
+
+public class MainController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hey");
+	}
+
+}
